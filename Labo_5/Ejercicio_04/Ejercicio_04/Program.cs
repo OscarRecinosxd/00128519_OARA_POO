ed8cc7a531fabf061650d99b579a02ce1a1f6e2f
OscarRecinosxd <00128519@uca.edu.sv>
2020-04-23 18:46:59 -0600

    add exercise 4

---
+++ Labo_5/Ejercicio_04/Ejercicio_04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ejercicio_04
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cadena de texto: ");
+            string Cadena = Console.ReadLine();
+            Console.Write("Ingrese el caracter 'a' que quiere reemplazar: ");
+            char a = Convert.ToChar(Console.ReadLine());
+            Console.Write("Ingrese el caracter 'b' por el cual va a reemplazar el caracter 'a' :");
+            char b = Convert.ToChar(Console.ReadLine());
+            Console.Write(Cadena.Replace(a,b));
+        }
+    }
+}
