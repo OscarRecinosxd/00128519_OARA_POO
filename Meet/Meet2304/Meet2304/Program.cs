38674d4fdee623aa07b527272f0d9931b77531ed
OscarRecinosxd <00128519@uca.edu.sv>
2020-04-23 21:50:54 -0600

    add exercise

---
+++ Meet/Meet2304/Meet2304/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Meet2304.Properties;
+
+namespace Meet2304
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = new List<Persona>();
+            
+            p.Add(new Persona("Walter", "Rafael",20));
+            p.Add(new Persona("Batar",edad: 19 ));
+            p.Add(new Persona(edad: 22 ));
+            p.Add(new Persona(apellido:"Morales" ));
+            
+            Console.Write("Agregar nombre a buscar ");
+            String buscar = "Walter";
+            buscar = Console.ReadLine();
+            
+            p.ForEach(it =>
+            {
+                if (buscar.Equals(it.Nombre))
+                {
+                    Console.WriteLine("Nuevo nombre: ");
+                    it.Nombre=Console.ReadLine();
+                    Console.WriteLine(it.ToString());
+                }
+            });
+
+            int añadir = 12;
+            int árbol = 14;
+
+
+        }
+    }
+}
---
+++ Meet/Meet2304/Meet2304/Properties/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Meet2304.Properties
+{
+    public class Persona
+    {
+        private String nombre, apellido;
+        private int edad;
+
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+        
+        
+
+        public Persona(String nombre="nada", String apellido="nada", int edad=0)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(nombre)}: {nombre}, {nameof(apellido)}: {apellido}, {nameof(edad)}: {edad}";
+        }
+
+        public String Saludar(String saludoP)
+        {
+            return nombre + " " + saludoP;
+        }
+    }
+}
