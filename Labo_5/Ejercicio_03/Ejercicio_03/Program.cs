fe369f09772b49b9dd36e4bb906248b14321a461
OscarRecinosxd <00128519@uca.edu.sv>
2020-04-23 18:46:22 -0600

    add exercise 3

---
+++ Labo_5/Ejercicio_03/Ejercicio_03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejercicio_03
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("¿Hasta que numero quiere imprimir?  ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a=1;
+            Ascendente(n,a); a=n-1;
+            Descendente(n,a);
+        }
+        static void Ascendente(int n,int a) {
+            if (a == n + 1)
+                return;
+            else {
+                Console.Write(a + ",");
+                Ascendente(n, a + 1);
+            }
+        }
+        
+        static void Descendente(int n,int a) {
+            if (a == 0)
+                return;
+            else {
+                Console.Write(a + ",");
+                Descendente(n, a - 1);
+            }
+        }
+    }
+}
