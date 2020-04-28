9373bb677e9cd407b711a5f18f8f090145fdfde3
OscarRecinosxd <00128519@uca.edu.sv>
2020-04-28 15:11:16 -0600

    add labo 6

---
+++ Labo_6/Laboratorio_06/Laboratorio_06/CalcularNota.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laboratorio_06
+{
+    public class CalcularNota
+    {
+
+        
+         public static double Calcular(List<Evaluacion> ev)
+         {
+             double Nota=0 , Promedio=0;
+             foreach (var s in ev)
+             {
+                 Console.WriteLine("Ingrese la nota de " + s.Nombre);
+                 Nota = Convert.ToDouble(Console.ReadLine());
+                 Promedio += (Nota * ((s.Porcentaje) / 100.0));
+                 
+             }
+
+             return Promedio;
+         }
+    }
+}
